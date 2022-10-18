bc9456c3bc31347cfd7893ce32ecd88be9b1bd27
Mikhail E <romandviski@mail.ru>
2022-10-18 13:03:57 +0300

    4. Виртуальные функции
    Создайте абстрактный класс Vehicle, содержащий следующие методы:
    - чистая виртуальная функция print() с параметром типа std::ostream;
    - перегрузка operator<<;
    
    Перегрузите функцию print(), чтобы следующий код произвел следующий вывод.

--- Middle_Chapter_1.cpp
+++ Middle_Chapter_1.cpp
@@ -1,224 +1,178 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <iostream>
-//https://itnan.ru/post.php?c=1&p=308890
+#include <functional>
 
-
-
-class Vector
+/*
+class IErrorLog
 {
 public:
-	Vector()
-	{
-		x = 0;
-		y = 0;
-		z = 0;
-	}
+	virtual bool openlog(const char* filename) = 0;
+	virtual bool closeLog() = 0;
+	virtual bool writeError(const char* errorMessage) = 0;
 
-	Vector(float x, float y, float z)
-	{
-		this->x = x;
-		this->y = y;
-		this->z = z;
-	}
-
-	operator float()
-	{
-		return sqrt(x * x + y * y + z * z);
-	}
-
-	friend Vector operator+(const Vector& a, const Vector& b);
-	friend Vector operator-(const Vector& a, const Vector& b);
-	friend Vector operator*(const Vector& a, const int b);
-	friend bool operator>(const Vector& a, const Vector& b);
-	friend std::ostream& operator<<(std::ostream& out, const Vector& v);
-	friend std::istream& operator>>(std::istream& stream, Vector& v);
-
-
-	float operator[](int index)
-	{
-		switch (index)
-		{
-		case 0:
-			return x;
-			break;
-		case 1:
-			return y;
-			break;
-		case 2:
-			return z;
-			break;
-		default:
-			std::cout << "index error";
-			return 0;
-		}
-	}
-
-private:
-	float x;
-	float y;
-	float z;
+	virtual ~IErrorLog() {};
 };
 
-Vector operator+(const Vector& a, const Vector& b)
+class FileErrorLog : public IErrorLog
 {
-	return Vector(a.x + b.x, a.y + b.y, a.z + b.z);
-}
+	
+};
 
-Vector operator-(const Vector& a, const Vector& b)
+class ConsoleErrorLog : public IErrorLog
 {
-	return Vector(a.x - b.x, a.y - b.y, a.z - b.z);
-}
+	
+};
+*/
 
-Vector operator*(const Vector& a, const int b)
+class Wheel
 {
-	return Vector(a.x * b, a.y * b, a.z * b);
-}
+public:
+	float diameter;
+	Wheel(float f)
+	{
+		diameter = f;
+	}
+};
 
-bool operator>(const Vector& a, const Vector& b)
+class Engine
 {
+public:
+	float power;
+	Engine(float f)
+	{
+		power = f;
+	}
+};
 
-	return false;
-}
-
-std::ostream& operator<<(std::ostream& out, const Vector& v)
+class Vehicle
 {
-	out << v.x << ' ' << v.y << ' ' << v.z;
+public:
+	Vehicle()
+	{
+	}
+	friend std::ostream& operator<<(std::ostream& out, const Vehicle& vehicle);
+
+	virtual void print(std::ostream& out) = 0;
+	virtual float GetPowerEngine() = 0;
+};
+
+std::ostream& operator<<(std::ostream& out, Vehicle& vehicle)
+{
+	vehicle.print(out);
 	return out;
 }
 
-std::istream& operator>>(std::istream& stream, Vector& v)
+class WaterVehicle : public Vehicle
 {
-	std::cout << "Input vector3:\nX:";
-	std::cin >> v.x;
-	std::cout << "Y:";
-	std::cin >> v.y;
-	std::cout << "Z:";
-	std::cin >> v.z;
-	std::cout << '\n';
-	return stream;
+	Engine* engine;
+protected:
+	float draught;
+public:
+	WaterVehicle(Engine engi, float value)
+	{
+		engine = new Engine(engi);
+		draught = value;
+	}
+
+	void print(std::ostream& out) override {
+		out << "WaterVehicle";
+	}
+
+	float GetPowerEngine() override {
+		return engine->power;
+	}
+};
+
+class RoadVehicle : public Vehicle
+{
+protected:
+	Engine* engine;
+	float clearance;
+public:
+	RoadVehicle(Engine engine, float clearance)
+	{
+		this->engine = new Engine(engine);
+		this->clearance = clearance;
+	}
+
+	void print(std::ostream& out) override {
+		out << "RoadVehicle";
+	}
+
+	float GetPowerEngine() override {
+		return engine->power;
+	}
+};
+
+class Bicycle : public RoadVehicle
+{
+	Wheel* frontWheel;
+	Wheel* backWheel;
+public:
+	Bicycle(Engine engine, Wheel wheel1, Wheel wheel2, float clearance) : RoadVehicle(engine, clearance)
+	{
+		frontWheel = new Wheel(wheel1);
+		backWheel = new Wheel(wheel2);
+	}
+
+	void print(std::ostream& out) override
+	{
+		out << "Bicycle Wheels: " << frontWheel->diameter << " " << backWheel->diameter;
+		out << " Ride height : " << clearance;
+	}
+};
+
+class Car : public RoadVehicle
+{
+	Wheel* frontRightWheel;
+	Wheel* frontLeftWheel;
+	Wheel* backRightWheel;
+	Wheel* backLeftWheel;
+public:
+	Car(Engine engine, Wheel wheel1, Wheel wheel2, Wheel wheel3, Wheel wheel4, float value) : RoadVehicle(engine, value)
+	{
+		frontRightWheel = new Wheel(wheel1);
+		frontLeftWheel = new Wheel(wheel2);
+		backRightWheel = new Wheel(wheel3);
+		backLeftWheel = new Wheel(wheel4);
+	}
+
+	void print(std::ostream& out) override
+	{
+		out << "Car Engine : " << engine->power;
+		out << " Wheels : " << frontRightWheel->diameter << " " << frontLeftWheel->diameter << " " << backRightWheel->diameter << " " << backLeftWheel->diameter;
+		out << " Ride height : " << clearance;
+	}
+};
+
+float GetHighestPower(std::vector<Vehicle*> vehicles)
+{
+	if (vehicles.size() == 0) return 0;
+	float maxEngine = 0;
+	for (int i = 0; i < vehicles.size(); i++)
+		if (vehicles[i]->GetPowerEngine() > maxEngine)
+			maxEngine = vehicles[i]->GetPowerEngine();
+	if (maxEngine == 0) return 0;
+	return maxEngine;
 }
-
-class Item
-{
-	std::string ItemName;
-public:
-	Item()
-	{
-		ItemName = "None";
-	}
-	Item(std::string inItemName)
-	{
-		ItemName = inItemName;
-	}
-	std::string ShowItemName()
-	{
-		//std::cout << ItemName << std::endl;
-		return ItemName;
-	}
-};
-
-class Guild;
-
-class Player
-{
-	Vector Location;
-	std::string PlayerName;
-	Item* MainItem;
-	std::vector<Guild*> Guilds;
-public:
-	Player()
-	{
-		PlayerName = "none";
-	}
-
-	Player(std::string inPlayerName, Vector InitLocation)
-	{
-		PlayerName = inPlayerName;
-		Location = InitLocation;
-	}
-
-	void ShowPlayerInfo()
-	{
-		std::cout << PlayerName << " is at " << Location << std::endl;
-		if (MainItem)
-		{
-			std::cout << PlayerName << " have " << MainItem->ShowItemName() << std::endl;
-		}
-		else
-		{
-			std::cout << PlayerName << " dont have item" << std::endl;
-		}
-	}
-	void SetNewItem(Item* newItem)
-	{
-		MainItem = newItem;
-	}
-
-	void JoinGuild(Guild* guildToJoin)
-	{
-		Guilds.push_back(guildToJoin);
-	}
-
-	friend class Guild;
-};
-
-class Guild
-{
-	std::vector<Player*> Players;
-public:
-	void AddNewPlayer(Player* newPlayer)
-	{
-		newPlayer->JoinGuild(this);
-		Players.push_back(newPlayer);
-	}
-
-	void RemovePlayer(Player* PlayerToRemove)
-	{
-		//Players.push_back(PlayerToRemove);
-	}
-};
-
-class PlayerParty
-{
-	Player** players;
-	int CurrentPlayersNum;
-public:
-	PlayerParty()
-	{
-		CurrentPlayersNum = 0;
-		players = new Player * [4];
-	}
-	void AddPlayerToParty(Player* newPlayer)
-	{
-		players[CurrentPlayersNum] = newPlayer;
-		CurrentPlayersNum++;
-	}
-	void ShowAllPlayersInfo()
-	{
-		for (int i = 0; i < CurrentPlayersNum; i++)
-		{
-			players[i]->ShowPlayerInfo();
-		}
-	}
-};
 
 int main()
 {
-	Player* PlayerA = new Player("Vasyan", Vector(4,0,4));
-	Player* PlayerB = new Player("Petyan", Vector(7, 7, 7));
-	Item* Sword = new Item("Sword");
-	PlayerA->SetNewItem(Sword);
-	std::cout << std::endl;
+	//first part
+	Car c(Engine(150), Wheel(17), Wheel(17), Wheel(18), Wheel(18), 150);
+	std::cout << c << std::endl;
 
-	PlayerParty* MainParty = new PlayerParty;
-	MainParty->AddPlayerToParty(PlayerA);
-	MainParty->AddPlayerToParty(PlayerB);
-	MainParty->ShowAllPlayersInfo();
-	std::cout << std::endl;
+	Bicycle b(Engine(150), Wheel(20), Wheel(20), 300);
+	std::cout << b << std::endl;
 
-	std::cout << std::endl << "===============================" << std::endl;
-	//system("pause");
+	//second part
+	std::vector<Vehicle*> v;
+	v.push_back(new Car(Engine(150), Wheel(17), Wheel(17), Wheel(18), Wheel(18), 250));
+	v.push_back(new Bicycle(Engine(250), Wheel(1), Wheel(1), 7));
+	v.push_back(new Car(Engine(200), Wheel(19), Wheel(19), Wheel(19), Wheel(19), 130));
+	v.push_back(new WaterVehicle(Engine(150), 5000));
+
+	std::cout << "The highest power is " << GetHighestPower(v) << std::endl;
+
 	return 0;
 }
